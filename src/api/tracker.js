1fdfacdae25ae21e2a6bdb663584748beca2f3cf
Aditya Kumar Jha <adityajha126@gmail.com>
2020-01-03 07:53:39 +0530

    complete app

---
+++ src/api/tracker.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { AsyncStorage } from 'react-native';
+
+let url;
+if (__DEV__) {
+	url = 'https://17e1e534.ngrok.io';
+} else {
+	url = 'https://sleepy-savannah-10606.herokuapp.com';
+}
+
+const instance = axios.create({
+	baseURL: url
+});
+
+instance.interceptors.request.use(
+	async (config) => {
+		const token = await AsyncStorage.getItem('token');
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(err) => {
+		return Promise.reject(err);
+	}
+);
+
+export default instance;
