4a1a4a8293de37a48bcc6f6532a1cca73cbd38da
Aditya Kumar Jha <adityajha126@gmail.com>
2020-01-03 09:17:31 +0530

    sdk expo

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 let url;
-if (__DEV__) {
-	url = 'https://17e1e534.ngrok.io';
-} else {
-	url = 'https://sleepy-savannah-10606.herokuapp.com';
-}
+// if (__DEV__) {
+// url = 'https://17e1e534.ngrok.io';
+// } else {
+url = 'https://path-track-app.herokuapp.com';
+// }
 
 const instance = axios.create({
 	baseURL: url
